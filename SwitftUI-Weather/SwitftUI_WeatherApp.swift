767024592257d65da45eead47dda88e5fc28910c
Saugat Rai <saugat@evolveasia.co>
2022-02-17 10:51:34 +0545

    Initial Commit

---
+++ SwitftUI-Weather/SwitftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitftUI_WeatherApp.swift
+//  SwitftUI-Weather
+//
+//  Created by Evolve on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwitftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
